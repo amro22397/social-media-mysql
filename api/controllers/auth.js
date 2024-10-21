96b9f5beb11fb821373b345181a608fbb9e772bb
amro22397 <amroalmutasim22@gmail.com>
2024-10-21 16:11:22 +0400

    register api

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,14 +1,36 @@
-import { db } from "../connect";
+import { db } from "../connect.js";
+import bcrypt from "bcryptjs";
 
-export const register = async (req, res) => {
-
-    q = "SELECT FROM users WHERE username = ?";
-
+export const register = (req, res) => {
+    //CHECK USER IF EXISTS
+  
+    const q = "SELECT * FROM users WHERE username = ?";
+  
     db.query(q, [req.body.username], (err, data) => {
-        if (err) return res.status.json(err);
-        if (data.length) return res.status(409).json("User already exists!")
-    } )
-}
+      if (err) return res.status(500).json(err);
+      if (data.length) return res.status(409).json("User already exists!");
+      //CREATE A NEW USER
+      //Hash the password
+      const salt = bcrypt.genSaltSync(10);
+      const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+  
+      const q =
+        "INSERT INTO users (`username`,`email`,`password`,`name`) VALUE (?)";
+  
+      const values = [
+        req.body.username,
+        req.body.email,
+        hashedPassword,
+        req.body.name,
+      ];
+  
+      db.query(q, [values], (err, data) => {
+        if (err) return res.status(500).json(err);
+        return res.status(200).json("User has been created.");
+      });
+    });
+  };
+  
 
 export const login = async (req, res) => {
     
--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 
-import express from 'express'
-import authRoutes from './routes/users.js'
+import express from "express";
+import authRoutes from './routes/auth.js'
 import userRoutes from './routes/users.js'
-import postRoutes from './routes/users.js'
-import commentRoutes from './routes/users.js'
-import likeRoutes from './routes/users.js'
+import postRoutes from './routes/posts.js'
+import commentRoutes from './routes/comments.js'
+import likeRoutes from './routes/likes.js'
 
 
 const app = express();
+
 
 
 app.use('/api/users', userRoutes);
@@ -19,5 +20,5 @@ app.use('/api/likes', likeRoutes);
 app.use(express.json());
 
 app.listen(8800, () => {
-    console.log("API working!")
-})
+    console.log("API working!");
+  });
